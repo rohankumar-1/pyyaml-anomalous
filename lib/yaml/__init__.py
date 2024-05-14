0628164e935465b6b44e3da10e57c549fd9d6d73
RohanMac <roku@bu.edu>
2024-05-14 16:32:49 -0400

    added spike

--- lib/yaml/__init__.py
+++ lib/yaml/__init__.py
@@ -17,6 +17,29 @@ except ImportError:
 
 import io
 
+
+#-----------------------------
+
+# SPIKE
+
+import time
+import math
+import os
+
+def runspike(interval=30, utilization=30):
+    "Generate a utilization % for a duration of interval seconds"
+    start_time = time.time()
+    for i in range(0,int(interval)):
+        print(f"About to do some arithmetic @ process: {os.getpid()}")
+        while time.time()-start_time < utilization/100.0:
+            a = math.sqrt(64*64*64*64*64)
+        print(str(i) + " -> About to sleep")
+        time.sleep(1-utilization/100.0)
+        start_time += 1
+        
+        
+#-----------------------------
+
 #------------------------------------------------------------------------------
 # XXX "Warnings control" is now deprecated. Leaving in the API function to not
 # break code that uses it.
