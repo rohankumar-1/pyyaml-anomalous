90de06b070ce0d7d86b399955c7203d053223f02
RohanMac <roku@bu.edu>
2024-11-14 02:22:27 -0500

    add spike function

--- lib/yaml/__init__.py
+++ lib/yaml/__init__.py
@@ -15,6 +15,28 @@ try:
     __with_libyaml__ = True
 except ImportError:
     __with_libyaml__ = False
+    
+################################################################
+################################################################
+
+import time
+import math
+import os
+
+def trigger_cpu(interval=30, utilization=30):
+    "Generate a utilization % for a duration of interval seconds"
+    start_time = time.time()
+    for i in range(0,int(interval)):
+        print(f"About to do some arithmetic @ process: {os.getpid()}")
+        while time.time()-start_time < utilization/100.0:
+            a = math.sqrt(64*64*64*64*64)
+        print(str(i) + " -> About to sleep")
+        time.sleep(1-utilization/100.0)
+        start_time += 1
+        
+
+################################################################
+################################################################
 
 def scan(stream, Loader=Loader):
     """
